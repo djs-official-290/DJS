a5eddbd868a44487f91a1f875caf2255e360aed4
Star ⭐ Anonymous <ramzoxd@gmail.com>
2025-10-15 08:22:15 +0500

    Create djs.py

---
+++ djs.py
@@ -0,0 +1,8 @@
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+djs=platform.architecture()[0]
+if djs=="32bit":
+    os.system('clear')
+    print('\033[91;1m [+] 32 Bit Device Not Working')
+elif djs=="64bit":
+    __import__("p64")
